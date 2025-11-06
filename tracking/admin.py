9e0263ba782e015450ca65371700c416f9fb6cc1
Adrian <adrian.silvasantiste@tecsup.edu.pe>
2025-11-05 19:33:57 -0500

    implementacino de gesiton de dispositivos en la api 2

--- tracking/admin.py
+++ tracking/admin.py
@@ -3,7 +3,11 @@ from .models import Location
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'latitude', 'longitude', 'timestamp')
+    list_display = ('get_user', 'latitude', 'longitude', 'timestamp')
     list_filter = ('timestamp',)
-    search_fields = ('user__nombre', 'user__email')
+    search_fields = ('device__user__email',)
+
+    def get_user(self, obj):
+        return obj.device.user
+    get_user.short_description = 'User'
     readonly_fields = ('timestamp',)
