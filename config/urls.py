8247b3f2a6b09e48f82e54ddf097e86a20652e29
adrianjsm79 <160637818+adrianjsm79@users.noreply.github.com>
2025-12-02 02:34:58 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/tracking/', include('tracking.urls')),
     path('api/devices/', include('devices.urls')),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
