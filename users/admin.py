75aa38831fe656d6564b9da7c4d81057b9ac366d
adrianjsm79 <160637818+adrianjsm79@users.noreply.github.com>
2025-12-05 08:19:43 -0500

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+# Registra el modelo para que aparezca en el panel
+@admin.site.register(LegalDocument) 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
